21b4534ef7c83e3c081b9d7f580422d06b53bed1
Olimov11 <jaloliddinolimov@mail.ru>
2022-10-12 10:03:36 +0500

    travelling

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var elWrapper = document.querySelector(".wrapper")
+
+var userMoney = prompt('How much money you have got?');
+
+var usd = "11.120";
+var euro = "10.900";
+
+var planeUsd = "500"
+var hotelUsd = "250"
+var enjoyEuro = "120"
+
+var planeSumm = planeUsd * usd;
+var hotelSumm = hotelUsd * usd;
+var enjoySumm = enjoyEuro * euro;
+
+var allCost = planeSumm * hotelSumm * enjoySumm
+
+if (userMoney >= allCost) {
+   elWrapper.textContent = "Have a safe trip";
+} else {
+   elWrapper.textContent = "You don't have enough money!";
+}
